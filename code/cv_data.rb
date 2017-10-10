edfa8a443d538125ae804b8d8383ef7f026c1491
Eli Thorkelson <eli@uchicago.edu>
2017-10-10 09:55:22 +0200

    Refactor CVData to be completely generalized

--- code/cv_data.rb
+++ code/cv_data.rb
@@ -1,55 +1,34 @@
 require 'yaml'
 require 'ostruct'
 
-class CVData
-  def education
-    @education ||= load_objects('../data/education.yml')
-  end
+DATA_PATH = File.expand_path('../data', File.dirname(__FILE__))
 
-  def fieldwork
-    @fieldwork ||= load_objects('../data/fieldwork.yml')
-  end
+# This class is really misnamed -- it actually is completely
+# general and will transform any folder of valid YAML data files
+# into a data object with a sub-object per YAML file. It assumes that
+# each file is a valid ruby hash key and that each file contains
+# a 1-dimensional array of either simple values (strings) or
+# key-value dictionaries. If it finds the latter it will transform
+# the list items into OpenStructs as well, for ease of access
+# in (e.g.) templating code.
 
-  def honors
-    @honors ||= load_objects('../data/honors.yml')
-  end
+class CVData < OpenStruct
 
-  def jobs
-    @jobs ||= load_objects('../data/employment.yml')
-  end
+  def load
+    Dir.chdir DATA_PATH
+    Dir["*.yml"].each do |name|
+      data = YAML.load_file name
+      data = structify(data) if data.first.is_a? Hash
 
-  def languages
-    @languages ||= load_strings('../data/languages.yml')
-  end
-
-  def machine_languages
-    @machine_languages ||= load_strings('../data/machine_languages.yml')
-  end
-
-  def organizing
-    @organizing ||= load_objects('../data/scholarly-organization.yml')
-  end
-
-  def presentations
-    @presentations ||= load_objects('../data/presentations.yml')
-  end
-
-  def publications
-    @publications ||= load_objects('../data/publications.yml')
-  end
-
-  def reviews
-    @reviews ||= load_strings('../data/reviews.yml')
+      key = name[0..-5] # strip off suffix to get key
+      self[key.to_sym] = data
+    end
   end
 
   protected
 
-  def load_strings(path)
-    YAML.load_file(path)
-  end
-
-  def load_objects(path)
-    YAML.load_file(path).map do |item|
+  def structify(hash_list)
+    hash_list.map do |item|
       OpenStruct.new(item)
     end
   end
